e39c7734094abb496b9481cb62333eff206a8d78
Fabien LOISON <flo@flogisoft.com>
2010-12-29 17:18:00 +0100

    * Credits now use the global refresh + animated

--- src/credits.c
+++ src/credits.c
@@ -30,6 +30,87 @@
 #include "credits.h"
 
 
+DM_Credits* new_credits(int numb_pages)
+{
+	DM_Credits *credits = malloc(sizeof(DM_Credits));
+	if (credits == NULL)
+	{
+		fprintf(stderr, "E: Cannot allocate memory.");
+		exit(EXIT_FAILURE);
+	}
+	credits->pages = malloc(numb_pages * sizeof(SDL_Surface*));
+	credits->titles = malloc(numb_pages * sizeof(SDL_Surface*));
+	credits->numb_pages = numb_pages;
+	credits->current_page = -1;
+	credits->speed = 20;
+	return credits;
+}
+
+
+void free_credits(DM_Credits *credits)
+{
+	int i;
+	for (i=0 ; i<credits->numb_pages ; i++)
+	{
+		SDL_FreeSurface(credits->pages[i]);
+		SDL_FreeSurface(credits->titles[i]);
+	}
+	free(credits->pages);
+	free(credits->titles);
+	free(credits);
+}
+
+
+void credits_cb(void *object, SDL_Surface *screen)
+{
+	DM_Credits *credits = object;
+	//Init a new page
+	if (credits->current_page < 0)
+	{
+		credits->current_page = 0 - credits->current_page;
+		credits->page_rect.x = screen->w;
+		credits->page_rect.y = 440;
+		credits->title_rect.x = 0 - credits->titles[credits->current_page-1]->w;
+		credits->title_rect.y = 400;
+		credits->speed = 20;
+	}
+	//blint
+	if (credits->page_rect.x < 0)
+	{
+		credits->page_rect.x = -800;
+	}
+	SDL_BlitSurface(credits->pages[credits->current_page-1], NULL, screen, &credits->page_rect);
+	SDL_BlitSurface(credits->titles[credits->current_page-1], NULL, screen, &credits->title_rect);
+	//Move
+	credits->page_rect.x -= credits->speed;
+	credits->title_rect.x += credits->speed / 2 + 1;
+	//Change speed
+	if (credits->page_rect.x < 17 * screen->w / 20) credits->speed = 15 ;
+	if (credits->page_rect.x < 16 * screen->w / 20) credits->speed = 11 ;
+	if (credits->page_rect.x < 15 * screen->w / 20) credits->speed =  8 ;
+	if (credits->page_rect.x < 14 * screen->w / 20) credits->speed =  6 ;
+	if (credits->page_rect.x < 13 * screen->w / 20) credits->speed =  3 ;
+	if (credits->page_rect.x < 12 * screen->w / 20) credits->speed =  2 ;
+	if (credits->page_rect.x < 11 * screen->w / 20) credits->speed =  1 ;
+	if (credits->page_rect.x <  6 * screen->w / 20) credits->speed =  2 ;
+	if (credits->page_rect.x <  5 * screen->w / 20) credits->speed =  5 ;
+	if (credits->page_rect.x <  4 * screen->w / 20) credits->speed = 10 ;
+	if (credits->page_rect.x <  3 * screen->w / 20) credits->speed = 15 ;
+	if (credits->page_rect.x <  2 * screen->w / 20) credits->speed = 20 ;
+	if (credits->page_rect.x <  1 * screen->w / 20) credits->speed = 30 ;
+	//Change of layer
+	if (credits->title_rect.x > screen->w)
+	{
+		credits->current_page += 1;
+		if (credits->current_page > credits->numb_pages)
+		{
+			credits->current_page = 1;
+		}
+		credits->current_page = 0 - credits->current_page;
+	}
+}
+
+
 /**
  * \fn void credits(SDL_Surface *screen)
  * \brief Display the credits.
@@ -39,92 +120,97 @@
  */
 void credits(SDL_Surface *screen)
 {
+	//Disable key repeat
+	SDL_EnableKeyRepeat(0, 0);
+	//Load sound
+	Mix_Chunk *sound_valid = load_sound_resource("menu_valid.wav");
+	//Background
+	DM_Surface *bg = load_resource_as_dm_surface("menu_bg.png");
+	int bg_refresh = ref_object(&layer_bg, bg, surface_refresh_cb);
+	//Title
+	DM_Surface *title = load_resource_as_dm_surface("main_menu_title.png");
+	int title_refresh = ref_object(&layer_bg, title, surface_refresh_cb);
 	//Create the menu
-/*	DM_Menu *menu = new_menu(*/
-/*			"Back",*/
-/*			"font_menu.png",*/
-/*			"font_menu_hl.png",*/
-/*			"cursor.png"*/
-/*			);*/
-/*	menu->menu_rect.x = screen->w - menu->menu->w - 20;*/
-/*	menu->menu_rect.y = screen->h - menu->menu->h - 15;*/
-/*	//Create the menu background*/
-/*	SDL_Surface *background_tmp = load_resource("menu_bg.png");*/
-/*	SDL_Surface *background = SDL_ConvertSurface(background_tmp, screen->format, NULL);*/
-/*	SDL_FreeSurface(background_tmp);*/
-/*	SDL_Surface *title = load_resource("main_menu_title.png");*/
-/*	SDL_BlitSurface(title, NULL, background, NULL);*/
-/*	SDL_FreeSurface(title);*/
-/*	//credits*/
-/*	SDL_Surface *credits_text = str_to_surface(*/
-/*			"font_main.png",*/
-/*			"DEVELOPERS:\n\n\*/
-/*			  Fabien LOISON\n\*/
-/*			  Mathilde BOUTIGNY\n\*/
-/*			  Vincent PEYROUSE\n\*/
-/*			  Germain CARRE\n\n\n\*/
-/*			GRAPHISTS:\n\n\*/
-/*			  Fabien LOISON\n\*/
-/*			  Mathilde BOUTIGNY\n\n\n\*/
-/*			Sounds:\n\n\*/
-/*			  Fabien LOISON"*/
-/*			);*/
-/*	SDL_Rect credits_text_rect = {*/
-/*		(screen->w - credits_text->w) / 4, //x*/
-/*		(screen->h - credits_text->h - 270 ) / 2 +270, //y*/
-/*		0, //w*/
-/*		0  //h*/
-/*		};*/
-/*	SDL_BlitSurface(credits_text, NULL, background, &credits_text_rect);*/
-/*	SDL_FreeSurface(credits_text);*/
-/*	//Sounds*/
-/*	Mix_Chunk *sound_valid = load_sound_resource("menu_valid.wav");*/
-/*	//Menu refresh and effects*/
-/*	DM_Menu_effect menu_effect;*/
-/*	menu_effect.bg = background;*/
-/*	menu_effect.menu = menu;*/
-/*	menu_effect.screen = screen;*/
-/*	menu_effect.timer = SDL_AddTimer(20, menu_glow_effect_cb, &menu_effect);*/
-/*	//Disable key repeat*/
-/*	SDL_EnableKeyRepeat(0, 0);*/
-/*	//Main loop*/
-/*	SDL_Event event;*/
-/*	int selected = -1;*/
-/*	do*/
-/*	{*/
-/*		SDL_WaitEvent(&event);*/
-/*		if (event.type == SDL_KEYDOWN)*/
-/*		{*/
-/*			switch (event.key.keysym.sym)*/
-/*			{*/
-/*				case SDLK_ESCAPE:*/
-/*					selected = menu->selected;*/
-/*					break;*/
-/*				case SDLK_SPACE:*/
-/*					selected = menu->selected;*/
-/*					break;*/
-/*				case SDLK_RETURN:*/
-/*					selected = menu->selected;*/
-/*					break;*/
-/*				case SDLK_KP_ENTER:*/
-/*					selected = menu->selected;*/
-/*					break;*/
-/*			}*/
-/*		}*/
-/*	}*/
-/*	while (selected < 0);*/
-/*	//Change the menu_effect (blink)*/
-/*	SDL_RemoveTimer(menu_effect.timer);*/
-/*	menu_effect.timer = SDL_AddTimer(20, menu_blink_effect_cb, &menu_effect);*/
-/*	//Play the validation sound*/
-/*	Mix_PlayChannel(-1, sound_valid, 0);*/
-/*	SDL_Delay(500);*/
-/*	//Stop the menu_effect*/
-/*	SDL_RemoveTimer(menu_effect.timer);*/
-/*	//Free the memory*/
-/*	free_menu(menu);*/
-/*	SDL_FreeSurface(background);*/
-/*	Mix_FreeChunk(sound_valid);*/
+	DM_Menu *menu = new_menu(
+			"Back",
+			 "font_menu.png",
+			 "font_menu_hl.png",
+			 "cursor.png"
+			 );
+	menu->menu_rect.x = screen->w - menu->menu->w - 20;
+	menu->menu_rect.y = screen->h - menu->menu->h - 15;
+	int menu_refresh = ref_object(&layer_menu, menu, menu_glow_effect_cb);
+	//Credits
+	DM_Credits *credits = new_credits(4);
+	credits->titles[0] = str_to_surface("font_main.png", "- DEVELOPERS -");
+	credits->pages[0] = str_to_surface(
+			"font_main.png",
+			"Fabien LOISON\n\
+			Mathilde BOUTIGNY\n\
+			Vincent PEYROUSE\n\
+			Germain CARRE"
+			);
+	credits->titles[1] = str_to_surface("font_main.png", "- GRAPHISTS -");
+	credits->pages[1] = str_to_surface(
+			"font_main.png",
+			"Fabien LOISON\n\
+			Mathilde BOUTIGNY"
+			);
+	credits->titles[2] = str_to_surface("font_main.png", "- SOUNDS -");
+	credits->pages[2] = str_to_surface(
+			"font_main.png",
+			"Fabien LOISON"
+			);
+	credits->titles[3] = str_to_surface("font_main.png", "- MUSICS -");
+	credits->pages[3] = str_to_surface(
+			"font_main.png",
+			"Core Wizard\n\
+			Dark Master"
+			);
+	int credits_refresh = ref_object(&layer_active, credits, credits_cb);
+	//Main loop
+	SDL_Event event;
+	int selected = -1;
+	do
+	{
+		SDL_WaitEvent(&event);
+		if (event.type == SDL_KEYDOWN)
+		{
+			switch (event.key.keysym.sym)
+			{
+				case SDLK_ESCAPE:
+					selected = menu->selected;
+					break;
+				case SDLK_SPACE:
+					selected = menu->selected;
+					break;
+				case SDLK_RETURN:
+					selected = menu->selected;
+					break;
+				case SDLK_KP_ENTER:
+					selected = menu->selected;
+					break;
+			}
+		}
+	}
+	while (selected < 0);
+	//Play a confirmation sound and change the menu effect
+	Mix_PlayChannel(-1, sound_valid, 0);
+	deref_object(&layer_menu, menu_refresh);
+	menu_refresh = ref_object(&layer_menu, menu, menu_blink_effect_cb);
+	SDL_Delay(500);
+	//Dereference objects and free the memory
+	deref_object(&layer_bg, bg_refresh);
+	deref_object(&layer_bg, title_refresh);
+	deref_object(&layer_menu, menu_refresh);
+	deref_object(&layer_active, credits_refresh);
+	SDL_Delay(20);
+	free_dm_surface(bg);
+	free_dm_surface(title);
+	free_menu(menu);
+	free_credits(credits);
+	//Free sounds memory
+	Mix_FreeChunk(sound_valid);
 }
 
 
--- src/credits.h
+++ src/credits.h
@@ -28,10 +28,27 @@
 #include <SDL_mixer.h>
 #endif
 
+#include "refresh.h"
 #include "main_functions.h"
 #include "menu.h"
 
 
+typedef struct DM_Credits DM_Credits;
+struct DM_Credits
+{
+	int numb_pages;
+	int current_page;
+	int speed;
+	SDL_Surface **titles;
+	SDL_Rect title_rect;
+	SDL_Surface **pages;
+	SDL_Rect page_rect;
+};
+
+
+DM_Credits* new_credits(int numb_pages);
+void free_credits(DM_Credits *credits);
+void credits_cb(void *object, SDL_Surface *screen);
 void credits(SDL_Surface *screen);
 
 
